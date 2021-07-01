7c3352e9cf4318c9f536df992870ba644165a858
rajsheanj <rajsheanj@gmail.com>
2021-07-01 17:34:45 +0530

    Delete Snapshot From AMI and practice code in Python

--- compareList.py
+++ compareList.py
@@ -8,33 +8,30 @@ list1 = [{'snap-id': 'snap1234', 'size': 'NA', 'snap_name': 'NA'},
       {'snap-id': 'snp3234', 'size': 'NA', 'snap_name': 'NA'},
       {'snap-id': 'snap9993', 'size': 'NA', 'snap_name': 'NA'},
       {'snap-id': 'snap4321', 'size': 'NA', 'snap_name': 'NA'},
-       {'snap-id': 'snap8888', 'size': 'NA', 'snap_name': 'NA'}]
+         {'snap-id': 'snap2222', 'size': 'NA', 'snap_name': 'NA'}]
 list2 = [{'snap-id': 'snap1234', 'size': '20', 'snap_name': 'abc'},
       {'snap-id': 'snp3234', 'size': '30', 'snap_name': 'def'},
       {'snap-id': 'snap9993', 'size': '40', 'snap_name': 'ght'},
       {'snap-id': 'snap1222', 'size': 'NA', 'snap_name': ''},
       {'snap-id': 'snp3239', 'size': 'NA', 'snap_name': ''},
-      {'snap-id': 'snap9963', 'size': 'NA', 'snap_name': ''}]
+      {'snap-id': 'snap9993', 'size': 'NA', 'snap_name': ''},
+      {'snap-id': 'snap4321', 'size': 'NA', 'snap_name': ''}]
 
 res = []
-diff = []
+list3 = []
 
 rest1=[]
-for d2 in list1:
+for d1 in list1:
     found_flag = 0
-    for d1 in list2:
-        if d2['snap-id'] == d1['snap-id']:
+    for d2 in list2:
+        if d1['snap-id'] == d2['snap-id']:
             res.append(d2)
-            boo_result = 1
+            found_flag = 1
             break
-        elif d2['snap-id'] != d1['snap-id']:
-            rest1.append(d2)
-            boo_result = 1
-            break
-        if found_flag == 0:
-                diff.append(d2)
+    if found_flag == 0:
+        list3.append(d1)
 print("rest", res)
-print("not matching value 2", rest1)
+print("list3", list3)
 print("\n")
 #print("diff", diff)
 #print("remainig matching id", rest1)
---
+++ test11.py
@@ -0,0 +1,7 @@
+list1= [1,2,3,4,5]
+string1= "My name is Ask Python"
+tuple1=(11,22,33,44)
+
+print(5,4,11 in list1) #True
+print("is" in string1) #True
+print(44 in tuple1) #False
